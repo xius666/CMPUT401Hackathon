bdb2c7b24e96bc6fe132d5f8278b50b4ec4796d4
Joshua Sabet <sabet@ualberta.com>
2021-01-23 11:13:24 -0700

    added all countries to drop down

--- covidtracker/track/views.py
+++ covidtracker/track/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 # Create your views here.
-
 
 def base(request):
-    try:
-        country = request.GET['drop1']
-    except KeyError:
-        country = ""
-    if country == "---select a conntry---":
-        country = ""
+    url = "https://covid-193.p.rapidapi.com/countries"
 
-    return render(request, 'index.html', {'country': country})
+    headers = {
+        'x-rapidapi-key': "cbef3ad3damsh177a38ec66e4f95p1a51f3jsne5512dc00c01",
+        'x-rapidapi-host': "covid-193.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers)
+
+    countryList = list(response.text.split('[')[3].split(']')[0].split(','))
+
+    return render(request, 'index.html', {"country_list": countryList})
