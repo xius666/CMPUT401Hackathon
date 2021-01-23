2835ad1f398283dbd88da57e19f8906b3ffff611
shiyuxiu <xiushiyu666@gmail.com>
2021-01-23 10:36:58 -0700

    fetching data

--- covidtracker/track/views.py
+++ covidtracker/track/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+import requests
+import json
 # Create your views here.
 # Create your views here.
 
@@ -9,7 +10,21 @@ def base(request):
         country = request.GET['drop1']
     except KeyError:
         country = ""
-    if country == "---select a conntry---":
+    if country == "---select a country---":
         country = ""
+        result = {'new': '', 'active': '', 'critical': '',
+                  'recovered': '', '1M_pop': '', 'total': '', 'country': ''}
+    else:
+        url = "https://covid-193.p.rapidapi.com/statistics"
+        querystring = {"country": country}
+        headers = {
+            'x-rapidapi-key': "1e2f4f94dbmsh9fc0dcd6b9a82fap1516edjsn402832e9ca33",
+            'x-rapidapi-host': "covid-193.p.rapidapi.com"
+        }
 
-    return render(request, 'index.html', {'country': country})
+        response = requests.request(
+            "GET", url, headers=headers, params=querystring).json()
+        result = response['response'][0]['cases']
+        result['country'] = country
+        print(result)
+    return render(request, 'index.html', result)
